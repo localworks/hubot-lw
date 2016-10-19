c539e2b24639303261d9badfb575472705a6889d
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-10-19 11:53:40 +0900

    make it better

--- scripts/google_analytics.js
+++ scripts/google_analytics.js
@@ -6,6 +6,7 @@ var ChartjsNode = require('chartjs-node');
 var exec = require('child_process').exec;
 var Cron = require('cron').CronJob;
 var Promise = require('promise');
+var _ = require('lodash');
 var key = JSON.parse(fs.readFileSync('./hubot-9c61d00b1b55.json', 'utf8'));
 var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/analytics'], null);
 var analytics = google.analytics('v3');
@@ -49,14 +50,14 @@ var gaTask = function gaTask(token) {
 };
 
 var _rowsToData = function _rowsToData(rows) {
-    var thisWeek = rows.slice(-14);
-    var twoWeeksAgo = rows.slice(0, 14);
+    var thisWeek = rows.slice(-15);
+    var twoWeeksAgo = rows.slice(0, 15);
 
     var labels = thisWeek.map(function (item) {
         return item[0];
     });
     var datasets = [{
-        label: 'From 2 weeks ago',
+        label: 'This week',
         data: thisWeek.map(function (item) {
             return item[1];
         }),
@@ -64,7 +65,7 @@ var _rowsToData = function _rowsToData(rows) {
         borderColor: "rgba(75,192,192,1)",
         lineTension: 0
     }, {
-        label: 'From 3 weeks ago',
+        label: 'Last week',
         data: twoWeeksAgo.map(function (item) {
             return item[1];
         }),
@@ -78,6 +79,38 @@ var _rowsToData = function _rowsToData(rows) {
         labels: labels,
         datasets: datasets
     };
+};
+
+var _sessionMsg = function _sessionMsg(rows) {
+    var thisWeek = rows.slice(-7);
+    var lastWeek = rows.slice(8, -7);
+
+    var thisWeekSum = _.chain(thisWeek).map(function (item) {
+        return item[1];
+    }).map(_.parseInt).tap(function (item) {
+        console.log('This week: ' + item);
+    }).sum().tap(function (item) {
+        console.log('Sum of this week: ' + item);
+    });
+    var lastWeekSum = _.chain(lastWeek).map(function (item) {
+        return item[1];
+    }).map(_.parseInt).tap(function (item) {
+        console.log('Last week: ' + item);
+    }).sum().tap(function (item) {
+        console.log('Sum of last week: ' + item);
+    });
+
+    var rateOfIncrease = (thisWeekSum - lastWeekSum) / lastWeekSum * 100;
+    rateOfIncrease = rateOfIncrease.toFixed(2);
+    var lastDay = _.last(thisWeek)[1];
+
+    rateOfIncrease = rateOfIncrease > 0 ? '↑ ' + rateOfIncrease.toString() : '↓ ' + (-1 * rateOfIncrease).toString();
+    thisWeekSum = thisWeekSum.toLocaleString();
+    lastDay = lastDay.toLocaleString();
+
+    var msg = '- \u5408\u8A08\u30BB\u30C3\u30B7\u30E7\u30F3\u6570\uFF08\u9031\uFF09: ' + thisWeekSum + ' \uFF08' + rateOfIncrease + '%\uFF09\n- \u30BB\u30C3\u30B7\u30E7\u30F3\u6570\uFF08\u65E5\uFF09\uFF1A ' + lastDay;
+
+    return msg;
 };
 
 var _makeChart = function _makeChart(data) {
@@ -113,17 +146,25 @@ var _makeChart = function _makeChart(data) {
     });
 };
 
-module.exports = function (robot) {
-    new Cron('00 30 22 * * *', function () {
+var _main = function _main(robot) {
+    return function () {
         authTask().then(function (result) {
             return gaTask(result.access_token);
         }).then(function (respond) {
             console.log(JSON.stringify(respond));
 
+            var msg = _sessionMsg(respond.rows);
+            robot.send({ room: room }, msg);
             var data = _rowsToData(respond.rows);
             _makeChart(data);
         }).catch(function (err) {
             console.log(err);
         });
-    }).start();
+    };
+};
+
+module.exports = function (robot) {
+    new Cron('00 30 22 * * *', _main(robot)).start();
+
+    //robot.respond(/ga$/i, _main(robot))
 };
